7a1eec32a60cf8a4e2cd3c1146816896d837da45
Adriana <adrianascosta9@gmail.com>
2022-10-24 17:29:27 -0300

    exerciciodebug

--- src/components/CardGrande/CardGrande.js
+++ src/components/CardGrande/CardGrande.js
@@ -1,16 +1,19 @@
+
 import React from 'react';
-import styled from 'styled components'
+//import styled from 'styled-components'
+import {ImgCardGrande, LayoutCardGrande, TituloCardGrande,InfosCard} from "./styles";
 
 
 function CardGrande(props) {
-   
-    return (
-        <LayoutCardGrande>
-            <ImgCardGrande src={ props.imagem } />
-            <InfosCardGrande>
-                <TituloCardGrande>{ props.nome }</TituloCardGrande>
-                <p>{ props.perfil.descricao }</p>
-            </InfosCardGrande>
-        </LayoutCardGrande>
-    )
+
+return (
+<LayoutCardGrande>
+<ImgCardGrande src={ props.imagem }  />
+<InfosCard>
+<TituloCardGrande>{ props.nome }</TituloCardGrande>
+<p>{ props.descricao }</p>
+</InfosCard>
+</LayoutCardGrande>
+);
 }
+export default CardGrande 
--- src/components/ImagemButton/ImagemButton.js
+++ src/components/ImagemButton/ImagemButton.js
@@ -1,23 +1,8 @@
 import React from 'react';
 // import './ImagemButton.css'
-import styled from 'styled-components'
+import {ContainerImagemVerMais,ImgVerMais} from"./styles.js";
 
 function ImagemButton(props) {
-
-    const ContainerImagemVerMais = styled.div`
-    display: flex;
-    align-items: center;
-    border: 1px solid black;
-    border-radius: 50px;
-    width: 200px;
-    padding: 15px 30px;
-    margin: 10px auto;
-    `
-
-    const ImgVerMais = styled.img`
-    width: 30px;
-    margin-right: 10px;
-    `
 
     return (
         <ContainerImagemVerMais>
@@ -25,7 +10,7 @@ function ImagemButton(props) {
             <p>{ props.texto }</p>
         </ContainerImagemVerMais>
 
-    )
+    );
 }
 
 export default ImagemButton;
